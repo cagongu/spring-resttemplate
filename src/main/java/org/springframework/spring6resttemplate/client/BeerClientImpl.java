3882d88160f6b74f18aa329b14121b94c6ed3141
cagongu <hophucthai9@gmail.com>
2024-02-05 01:55:55 +0700

    toi dang co loi tu choi ket noi chua fix duoc

--- src/main/java/org/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/org/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,11 +1,28 @@
 package org.springframework.spring6resttemplate.client;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.spring6resttemplate.model.BeerDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<String> stringResponse =
+                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+
+        System.out.println(stringResponse.getBody());
+
         return null;
     }
 }
---
+++ src/test/java/org/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,18 @@
+package org.springframework.spring6resttemplate.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BeerClientImplTest {
+
+    @Autowired
+    BeerClientImpl beerClient;
+
+    @Test
+    void listBeers() {
+        beerClient.listBeers();
+    }
+}
