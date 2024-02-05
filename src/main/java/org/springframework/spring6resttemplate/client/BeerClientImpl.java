907c761679a2598f3c226f48ffa1d050782b51c3
cagongu <hophucthai9@gmail.com>
2024-02-05 12:34:24 +0700

    chinh sua phan trang

--- src/main/java/org/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/org/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,15 +1,13 @@
 package org.springframework.spring6resttemplate.client;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.spring6resttemplate.model.BeerDTO;
+import org.springframework.spring6resttemplate.model.BeerDTOPageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -18,25 +16,14 @@ public class BeerClientImpl implements BeerClient {
     private final RestTemplateBuilder restTemplateBuilder;
 
     private static final String BASE_URL = "http://localhost:8080";
-
     private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String> stringResponseEntity = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
-
-        ResponseEntity<Map> mapResponseEntity = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
-
-//        cach su dung jsonNode https://www.baeldung.com/java-jsonnode-get-keys
-        ResponseEntity<JsonNode> jsonNodeResponseEntity = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
-
-        jsonNodeResponseEntity.getBody().findPath("content").elements().forEachRemaining(node -> {
-            System.out.println(node.get("beerName").asText());
-        });
-
-        System.out.println(stringResponseEntity.getBody());
+        ResponseEntity<BeerDTOPageImpl> stringResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH , BeerDTOPageImpl.class);
 
         return null;
     }
